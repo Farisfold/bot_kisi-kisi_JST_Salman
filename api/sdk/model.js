e0578b205f64fb407f11cc11008de3ee0991bbe0
Farisfold <84666834+Farisfold@users.noreply.github.com>
2021-06-25 23:29:27 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/TehAnget-Stunggal/bot-delta/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Farisfold/bot_kisi-kisi_JST_Salman/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
