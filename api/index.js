74b75e98db6739d3629cefd31c4c130c8d21ab26
Farisfold <84666834+Farisfold@users.noreply.github.com>
2021-06-28 17:11:39 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1776524070:AAFT-axXNAMXssJdBSOVz1lfmT2rPgFaOBc'
+const token = '1778411965:AAF0x5ZWWlqJhledAZCdUDr-8FCgKNKYiSs'
 const bot = new TelegramBot(token, {polling: true});
 
 
